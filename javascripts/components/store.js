faba4a6a56f9b5e61f7687e1d366cada0458f6d1
Lwam Tekie <ltekie87@yahoo.com>
2019-04-09 18:54:30 -0500

    wip

--- javascripts/components/store.js
+++ javascripts/components/store.js
@@ -1,13 +1,23 @@
-import book from '../helpers/book.js'
-import util from '../helpers/util.js'
+import book from '../helpers/book.js';
+import util from '../helpers/util.js';
 
-const makeStore = () => {
+const addToCartEvent = (e) => {
+    e.preventDefault();
     const bookInfo = book.getBook();
-    let domstring = `<h3>Our Only Book:</h3>`
-    domstring += `<h3>${bookInfo.price}</h3>`
-    domstring += `<img src=${bookInfo.image} alt=book cover>`
-    domstring += ` <button class= "btn btn-danger">Add to cart</button>`
+    console.log('book:', bookInfo);
+};
+   
+    
+
+    const makeStore = () => {
+    const bookInfo = book.getBook();
+    let domstring = `<h3>Our Only Book:</h3>`;
+    domstring += `<h3>${bookInfo.price}</h3>`;
+   
+    domstring += `<img class="book-cover" src=${bookInfo.image} alt="book cover">`;
+    domstring += ` <button id="cart-button" class= "btn btn-danger">Add to cart</button>`;
     util.printToDom('store-container', domstring);
+    document.getElementById('cart-button').addEventListener('click', addToCartEvent);
 };
 
-export default {makeStore};
+export default { makeStore };
